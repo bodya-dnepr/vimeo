53201cec1b4226505e5164b1307e190a22936ea2
Bogdan Derevenets <dbodyas@gmail.com>
2015-07-15 17:40:01 +0300

    Add search_videos endpoint

--- lib/vimeo/endpoints/videos.rb
+++ lib/vimeo/endpoints/videos.rb
@@ -10,6 +10,11 @@ module Vimeo
       def video video_id
         perform_get_with_object("/videos/#{video_id}", {}, Vimeo::Entities::Video)
       end
+
+      def search_videos query, options = {}
+        options.merge! query: query
+        perform_get_with_object("/videos", options, Vimeo::Entities::Video)
+      end
     end
   end
 end

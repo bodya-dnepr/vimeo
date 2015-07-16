263e2bd18cbe4dcf93bb20ebef2401e15b34c558
Bogdan Derevenets <dbodyas@gmail.com>
2015-07-16 12:27:08 +0300

    Add parameters filtration in search_videos

--- lib/vimeo/endpoints/videos.rb
+++ lib/vimeo/endpoints/videos.rb
@@ -12,6 +12,8 @@ module Vimeo
       end
 
       def search_videos query, options = {}
+        available = [:page, :per_page, :sort, :direction, :filter]
+        options   = options.keep_if {|k,v| available.include?(k) && !v.nil? }
         options.merge! query: query
         perform_get_with_object("/videos", options, Vimeo::Entities::Video)
       end
